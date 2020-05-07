cabe836e0845302f22943610e90dd1e0419a72c6
pratamawijaya <set.mnemonix@gmail.com>
2020-05-07 21:46:42 +0700

    1 - wip homepage

--- ps5_product/lib/constants.dart
+++ ps5_product/lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kBlueColor = Color(0xFF2372F0);
 const kGreyColor = Color(0x33647082);
+const kGreyColor2 = Color(0xFF647082);
--- ps5_product/lib/main.dart
+++ ps5_product/lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:clay_containers/constants.dart';
+import 'package:clay_containers/widgets/clay_containers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:ps5_product/constants.dart';
 
 void main() => runApp(MyApp());
 
@@ -29,6 +32,109 @@ class HomeScreen extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           _BackgroundRight(width: size.width * 0.4, height: size.height * 0.8),
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 15,
+                ),
+                _AppBar(),
+                SizedBox(
+                  height: 30,
+                ),
+                _Title(
+                  text: "Featured Product",
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  final String text;
+
+  const _Title({Key key, this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final words = text.split(' ');
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: RichText(
+        text: TextSpan(children: [
+          TextSpan(
+            text: words[0],
+            style: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 36,
+              color: Colors.black,
+            ),
+          ),
+          TextSpan(text: "\n"),
+          TextSpan(
+              text: words[1],
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 36,
+                color: Colors.black,
+              )),
+        ]),
+      ),
+    );
+  }
+}
+
+class _AppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          ClayContainer(
+            width: 50,
+            height: 50,
+            depth: 20,
+            borderRadius: 25,
+            curveType: CurveType.concave,
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: kGreyColor, width: 2),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Icon(
+                Icons.menu,
+                color: kGreyColor2,
+                size: 25,
+              ),
+            ),
+          ),
+          ClayContainer(
+            width: 50,
+            height: 50,
+            depth: 20,
+            borderRadius: 25,
+            parentColor: kBlueColor,
+            curveType: CurveType.concave,
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: kGreyColor, width: 2),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Icon(
+                Icons.shopping_cart,
+                color: kGreyColor2,
+                size: 25,
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -48,8 +154,10 @@ class _BackgroundRight extends StatelessWidget {
       top: 0,
       height: height,
       child: ClipRRect(
-        child: ColoredBox()
-      ),
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20)),
+          child: ColoredBox(
+            color: kBlueColor,
+          )),
     );
   }
 }
